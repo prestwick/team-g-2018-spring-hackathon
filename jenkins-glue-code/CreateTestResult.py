f40fe8a0071d2398dd31e54298e82d1043b2ca0e
rnasir <ramsey.nasir@gmail.com>
2018-03-23 12:16:42 -0500

    Use correct timestamp

--- jenkins-glue-code/CreateTestResult.py
+++ jenkins-glue-code/CreateTestResult.py
@@ -1,8 +1,23 @@
 import requests
 import json
 import os
+import datetime
 
-r = requests.post('http://hack-g.amer.corp.natinst.com/nitestmonitor/v1/results', json = {"results":[{ "fileIds":[], "keywords": [ "2018springhackathon" ], "programName":"InsightCM Build", "startTime": "2018-3-23T:11:04:05.111Z", "status": {"statusType":"RUNNING", "statusName":"Running"}, "systemId":"2349F6E4-B0D9-4527-8638-FA8BFA0F5B41", "updatedAt":"2018-3-23T:11:04:05.111Z"}]}, auth=("admin", "hack-g"))
+timestamp = datetime.datetime.utcnow().isoformat()
+
+jsonRequest = { "results":[ 
+				{ 
+					"fileIds":[], 
+					"keywords": [ "2018springhackathon" ], 
+					"programName":"InsightCM Build", 
+					"startTime": timestamp,
+					"status": { "statusType":"RUNNING", "statusName":"Running" }, 
+					"systemId":"2349F6E4-B0D9-4527-8638-FA8BFA0F5B41", 
+					"updatedAt": timestamp
+				}]
+				}
+				
+r = requests.post('http://hack-g.amer.corp.natinst.com/nitestmonitor/v1/results', json = jsonRequest, auth=("admin", "hack-g"))
 
 if r.status_code == 201:
 	file_path = "C:/ProgramData/Hackathon/resultId.txt"
